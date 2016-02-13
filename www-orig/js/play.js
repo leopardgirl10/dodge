c42efcdedebc175555ec6a582cfd54f34c92fdf6
Rob Muhlestein <rob@skilstak.com>
2016-02-13 11:59:30 -0500

    save

---
+++ www-orig/js/play.js
@@ -0,0 +1,26 @@
+// State: DogeDodge.Play
+
+'use strict';
+
+DogeDodge.Play = function () {};
+
+DogeDodge.Play.prototype = {
+
+  init: function () {
+    this.background = null;
+  },
+
+  create: function () {
+    this.background = this.add.tileSprite(0,0,320,568,"background");
+    this.background.autoScroll(0,50);
+    this.background.scale.set(1);
+  },
+
+  update: function () {
+  },
+
+  end: function () {
+  }
+
+};
+
--- www/js/play.js
+++ www/js/play.js
@@ -1,25 +1,28 @@
 // State: DogeDodge.Play
+'use strict'
 
-'use strict';
+var DogeDodge = {};
 
 DogeDodge.Play = function () {};
 
 DogeDodge.Play.prototype = {
 
   init: function () {
-    this.background = null;
+    console.log("%c~~~ Booting DogeDodge ~~~\n Compliments of SkilStak",
+                "color:#fdf6e3; background:#073642");
+  },
+
+  preload: function () {
+    this.load.image('background','assets/background.png');
   },
 
   create: function () {
-    this.background = this.add.tileSprite(0,0,320,568,"background");
-    this.background.autoScroll(0,50);
+    this.background = this.add.tileSprite(0,0,320,568,'background');
+    this.background.autoScroll(0,-50);
     this.background.scale.set(1);
   },
 
   update: function () {
-  },
-
-  end: function () {
   }
 
 };
