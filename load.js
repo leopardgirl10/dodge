8c02e4e58998eaf644f1b4060241e920d18df3b1
Rob Muhlestein <rob@skilstak.com>
2016-07-28 11:22:19 -0400

    save

--- load.js
+++ load.js
@@ -5,7 +5,7 @@
 
     if (typeof Symbol == "undefined") return false;
     try {
-        eval("class Foo {}");
+        eval("sdfclass Foo {}");
         eval("var bar = (x) => x+1");
     } catch (e) { return false; }
 
