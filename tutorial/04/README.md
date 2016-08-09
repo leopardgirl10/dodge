9e81903f6b5bffcec8087b2554e5e84fa850ef8c
Rob Muhlestein <rob@skilstak.com>
2016-08-09 11:58:18 -0400

    save

--- tutorial/04/README.md
+++ tutorial/04/README.md
@@ -1,2 +1,7 @@
-# Setup Styles
+# Add Viewport Sizing
+
+1. Add `meta` tag
+1. Add `Boot` state
+1. Add `preload()` callback
+1. Add `scaleMode`, `pageAlignHorizontally`, and `PageAlignVertically` 
 
--- tutorial/04/dodge.js
+++ tutorial/04/dodge.js
@@ -1,1 +1,13 @@
+class BootState {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+}
+
 var game = new Phaser.Game(320.568);
+game.state.add("Boot",BootState);
+game.state.start("Boot");
+
+
