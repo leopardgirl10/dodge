8723be49095d8c16d05700c1cf85c3bc2ea0548b
Rob Muhlestein <rob@skilstak.com>
2016-08-10 17:04:01 -0400

    save

---
+++ tutorial/11/README.md
@@ -0,0 +1,4 @@
+# Load Our Player Spritesheet
+
+1. Load the `player.png` sprite in the `LoadState.preload()` method
+1. Add config `C` variables for `file`, `width`, `height`, `frames`
---
+++ tutorial/11/dodge.js
@@ -0,0 +1,67 @@
+var C = {
+  "game": {
+    "width": 320,
+    "height": 568
+  },
+  "bg": {
+    "width": 320,
+    "height": 568,
+    "xspeed": 0,
+    "yspeed": 700,
+    "file": "assets/background.png"
+  },
+  "p": {
+    "file": "assets/player.png",
+    "width": 46,
+    "height": 64,
+    "frames": 2
+  }
+}
+
+//---------------------------------------------------------
+
+class BootState {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+  create() {
+    this.state.start("Load")
+  }
+}
+
+class LoadState {
+  preload() {
+    console.log("Loading...");
+    this.load.image("background",C.bg.file)
+    this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames)
+  }
+  create() {
+    console.log("Loaded");
+    this.state.start("Play")
+  }
+}
+
+class PlayState {
+  create() {
+    console.log("Entered Play State");
+    this.background = this.add.tileSprite(0,0,
+                                  C.bg.width,C.bg.height,"background");
+    this.background.autoScroll(C.bg.xspeed,C.bg.yspeed);
+  }
+}
+
+//---------------------------------------------------------
+
+function restart() {
+  game.state.start("Boot");
+}
+
+var game = new Phaser.Game(C.game.width,C.game.height);
+game.state.add("Boot",BootState);
+game.state.add("Load",LoadState);
+game.state.add("Play",PlayState);
+game.state.start("Boot");
+
+
