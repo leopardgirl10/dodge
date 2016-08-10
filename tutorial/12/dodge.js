190976b7bba1f761efe3d84da1c6f6a3a747f7e9
Rob Muhlestein <rob@skilstak.com>
2016-08-10 17:18:16 -0400

    save

---
+++ tutorial/12/dodge.js
@@ -0,0 +1,73 @@
+var C = {
+  "game": {
+    "width": 320,
+    "height": 568
+  },
+  "bg": {
+    "width": 320,
+    "height": 568,
+    "xspeed": 0,
+    "yspeed": 700,
+    "file": "assets/background.png"
+  },
+  "p": {
+    "file": "assets/player.png",
+    "width": 46,
+    "height": 64,
+    "frames": 2,
+    "startx": 160,
+    "starty": 500 
+  }
+}
+
+//---------------------------------------------------------
+
+class BootState {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+  create() {
+    this.state.start("Load")
+  }
+}
+
+class LoadState {
+  preload() {
+    console.log("Loading...");
+    this.load.image("background",C.bg.file)
+    this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames);
+  }
+  create() {
+    console.log("Loaded");
+    this.state.start("Play")
+  }
+}
+
+class PlayState {
+  create() {
+    console.log("Entered Play State");
+    this.background = this.add.tileSprite(0,0,
+                                  C.bg.width,C.bg.height,"background");
+    this.background.autoScroll(C.bg.xspeed,C.bg.yspeed);
+    this.player = this.add.sprite(C.p.startx,C.p.starty,"player");
+    this.player.anchor.set(0.5,0.5);
+    this.player.smoothed = false;
+    this.player.scale.set(1);
+  }
+}
+
+//---------------------------------------------------------
+
+function restart() {
+  game.state.start("Boot");
+}
+
+var game = new Phaser.Game(C.game.width,C.game.height);
+game.state.add("Boot",BootState);
+game.state.add("Load",LoadState);
+game.state.add("Play",PlayState);
+game.state.start("Boot");
+
+
